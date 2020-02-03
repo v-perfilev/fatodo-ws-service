74e701644bbd029f3a24eb55084a95fbaac1a1b3
Vladimir Perfilev <persoff68@gmail.com>
2020-02-03 23:18:04 +0100

    add feign

--- src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
+++ src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
@@ -1,17 +1,14 @@
 package com.persoff68.fatodo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {
-        SecurityAutoConfiguration.class,
-        ManagementWebSecurityAutoConfiguration.class
-})
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients
 public class ExtendedSkeletonApplication {
 
     public static void main(String[] args) {
