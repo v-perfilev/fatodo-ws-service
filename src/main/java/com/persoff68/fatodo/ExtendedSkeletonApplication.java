476b9191ffe7916c3c0d77352e133d2a7b331008
Vladimir Perfilev <persoff68@gmail.com>
2020-02-03 21:31:24 +0100

    add config for db

--- src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
+++ src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
@@ -1,10 +1,16 @@
 package com.persoff68.fatodo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class
+})
 @EnableDiscoveryClient
 public class ExtendedSkeletonApplication {
 
---
+++ src/main/java/com/persoff68/fatodo/config/DatabaseConfiguration.java
@@ -0,0 +1,9 @@
+package com.persoff68.fatodo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories
+public class DatabaseConfiguration {
+}
