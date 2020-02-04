b77413f24941bd646a4284bfc5eb5613691efc3c
Vladimir Perfilev <persoff68@gmail.com>
2020-02-04 22:04:18 +0100

    edit mongo config

--- src/main/java/com/persoff68/fatodo/config/DatabaseConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/DatabaseConfiguration.java
@@ -1,9 +1,25 @@
 package com.persoff68.fatodo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 @EnableMongoRepositories
+@EnableMongoAuditing
 public class DatabaseConfiguration {
+
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
 }
