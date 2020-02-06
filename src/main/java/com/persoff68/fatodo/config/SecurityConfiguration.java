4bb767d4cf363cead3a829d1b4d2bc21ed097ceb
Vladimir Perfilev <persoff68@gmail.com>
2020-02-06 16:56:04 +0100

    add securityProblemSupport to SecurityConfiguration

--- src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
@@ -1,12 +1,19 @@
 package com.persoff68.fatodo.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 @EnableWebSecurity
+@Import(SecurityProblemSupport.class)
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final SecurityProblemSupport securityProblemSupport;
 
     protected void configure(final HttpSecurity http) throws Exception {
         http
@@ -16,7 +23,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .formLogin().disable()
-                .httpBasic().disable();
+                .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(securityProblemSupport)
+                .accessDeniedHandler(securityProblemSupport);
     }
 
 }
