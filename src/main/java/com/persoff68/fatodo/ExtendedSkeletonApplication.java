09802d1ae1f21da853be4bf0af22379f843c73e8
Vladimir Perfilev <persoff68@gmail.com>
2020-02-06 16:21:18 +0100

    remove unnecessary imports from main

--- src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
+++ src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
@@ -1,9 +1,7 @@
 package com.persoff68.fatodo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication(exclude = {
