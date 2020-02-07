fc3dfdda59bae67c90bcbdb2bd6d52bb30471c0b
Vladimir Perfilev <persoff68@gmail.com>
2020-02-08 00:25:47 +0100

    add profile constants

---
+++ src/main/java/com/persoff68/fatodo/constant/ProfileConstants.java
@@ -0,0 +1,8 @@
+package com.persoff68.fatodo.constant;
+
+public interface ProfileConstants {
+    String DEVELOPMENT = "dev";
+    String DOCKER = "docker";
+    String PRODUCTION = "prod";
+    String TEST = "test";
+}
