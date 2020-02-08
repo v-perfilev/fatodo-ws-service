c3f70c9f29a46b1d434c6cedf247a08261a68c2a
Vladimir Perfilev <persoff68@gmail.com>
2020-02-08 01:01:55 +0100

    add default profile util

--- src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
+++ src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
@@ -1,13 +1,19 @@
 package com.persoff68.fatodo;
 
+import com.persoff68.fatodo.config.AppProfileUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class ExtendedSkeletonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExtendedSkeletonApplication.class, args);
+        Properties defaultProfileProperties = AppProfileUtil.getDefaultProfile();
+        SpringApplication app = new SpringApplication(ExtendedSkeletonApplication.class);
+        app.setDefaultProperties(defaultProfileProperties);
+        app.run(args);
     }
 
 }
---
+++ src/main/java/com/persoff68/fatodo/config/AppProfileUtil.java
@@ -0,0 +1,17 @@
+package com.persoff68.fatodo.config;
+
+import com.persoff68.fatodo.constant.ProfileConstants;
+
+import java.util.Properties;
+
+public class AppProfileUtil {
+
+    private static final String SPRING_PROFILES_DEFAULT = "spring.profiles.default";
+
+    public static Properties getDefaultProfile() {
+        Properties properties = new Properties();
+        properties.put(SPRING_PROFILES_DEFAULT, ProfileConstants.DEVELOPMENT);
+        return properties;
+    }
+
+}
