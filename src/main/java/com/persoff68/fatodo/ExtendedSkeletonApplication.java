3d97634be55efeb911b2282ef319b286ecb5a58a
Vladimir Perfilev <persoff68@gmail.com>
2020-02-09 01:25:33 +0100

    move feign and discovery clients to main class

--- src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
+++ src/main/java/com/persoff68/fatodo/ExtendedSkeletonApplication.java
@@ -5,10 +5,14 @@ import com.persoff68.fatodo.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.Properties;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 @EnableConfigurationProperties(AppProperties.class)
 public class ExtendedSkeletonApplication {
 
