f5fd96928941551b48d42dfbb9db39d6a61f85d9
Vladimir Perfilev <persoff68@gmail.com>
2020-02-19 22:57:41 +0100

    create app constants

--- src/main/java/com/persoff68/fatodo/config/CloudConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/CloudConfiguration.java
@@ -1,5 +1,6 @@
 package com.persoff68.fatodo.config;
 
+import com.persoff68.fatodo.config.constant.AppConstants;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = AppConstants.FEIGN_CLIENT_PATH)
 @EnableConfigurationProperties(AppProperties.class)
 @ConditionalOnProperty(prefix = "spring", name = "cloud.disabled", matchIfMissing = true)
 public class CloudConfiguration {
---
+++ src/main/java/com/persoff68/fatodo/config/constant/AppConstants.java
@@ -0,0 +1,6 @@
+package com.persoff68.fatodo.config.constant;
+
+public interface AppConstants {
+    String REPOSITORY_PATH = "com.persoff68.fatodo.repository";
+    String FEIGN_CLIENT_PATH = "com.persoff68.fatodo.client";
+}
