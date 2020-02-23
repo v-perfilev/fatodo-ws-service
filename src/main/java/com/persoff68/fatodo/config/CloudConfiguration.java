fa2a493de2d31062af670997071c38be4a8cdc4b
Vladimir Perfilev <persoff68@gmail.com>
2020-02-23 22:52:29 +0100

    add hystrix

--- src/main/java/com/persoff68/fatodo/config/CloudConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/CloudConfiguration.java
@@ -1,13 +1,16 @@
 package com.persoff68.fatodo.config;
 
 import com.persoff68.fatodo.config.constant.AppConstants;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableFeignClients(basePackages = AppConstants.FEIGN_CLIENT_PATH)
 public class CloudConfiguration {
 }
