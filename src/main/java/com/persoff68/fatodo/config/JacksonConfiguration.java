c403986eb7e609278016e52d204dd98ba2b7df63
Vladimir Perfilev <persoff68@gmail.com>
2020-03-07 19:43:51 +0100

    add time module to jackson

--- src/main/java/com/persoff68/fatodo/config/JacksonConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/JacksonConfiguration.java
@@ -3,6 +3,7 @@ package com.persoff68.fatodo.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +18,16 @@ public class JacksonConfiguration {
     private final ConstraintViolationProblemModule constraintViolationProblemModule;
 
     @Bean
+    JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
+    }
+
+    @Bean
     ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(problemModule);
         objectMapper.registerModule(constraintViolationProblemModule);
+        objectMapper.registerModule(javaTimeModule());
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         return objectMapper;
