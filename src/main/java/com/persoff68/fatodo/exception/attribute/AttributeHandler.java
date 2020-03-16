f05286b521a2c9f26710a2dbedf953ca18d3b6ef
Vladimir Perfilev <persoff68@gmail.com>
2020-03-16 19:51:36 +0100

    add abstract error handler

--- src/main/java/com/persoff68/fatodo/exception/attribute/AttributeHandler.java
+++ src/main/java/com/persoff68/fatodo/exception/attribute/AttributeHandler.java
@@ -41,17 +41,23 @@ public class AttributeHandler {
         return attributeStrategy.getStatus();
     }
 
-    public ResponseEntity<String> getResponseEntity(ObjectMapper objectMapper) throws IOException {
+    public ResponseEntity<String> getResponseEntity(ObjectMapper objectMapper) {
         HttpStatus status = getStatus();
-        Map<String, Object> errorAttributes = getErrorAttributes();
-        String responseBody = objectMapper.writeValueAsString(errorAttributes);
-        return ResponseEntity.status(status).body(responseBody);
+        String body = formJson(getErrorAttributes(), objectMapper);
+        return ResponseEntity.status(status).body(body);
     }
 
     public void sendError(ObjectMapper objectMapper, HttpServletResponse response) throws IOException {
         HttpStatus status = getStatus();
-        Map<String, Object> errorAttributes = getErrorAttributes();
-        String responseBody = objectMapper.writeValueAsString(errorAttributes);
-        response.sendError(status.value(), responseBody);
+        String body = formJson(getErrorAttributes(), objectMapper);
+        response.sendError(status.value(), body);
+    }
+
+    private String formJson(Map<String, Object> errorAttributes, ObjectMapper objectMapper) {
+        try {
+            return objectMapper.writeValueAsString(errorAttributes);
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
--- src/main/java/com/persoff68/fatodo/exception/handler/ExceptionHandling.java
+++ src/main/java/com/persoff68/fatodo/exception/handler/ExceptionHandling.java
@@ -1,11 +1,27 @@
 package com.persoff68.fatodo.exception.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.persoff68.fatodo.exception.AbstractException;
+import com.persoff68.fatodo.exception.attribute.AttributeHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@RequiredArgsConstructor
 public class ExceptionHandling {
+
+    private final ObjectMapper objectMapper;
+
+    @ExceptionHandler(AbstractException.class)
+    public ResponseEntity<String> handleAbstractException(HttpServletRequest request, AbstractException e) {
+        return AttributeHandler.from(request, e).getResponseEntity(objectMapper);
+    }
 
 }
