07fc35154fdef2064de510a4f1353d82a947f5de
Vladimir Perfilev <persoff68@gmail.com>
2020-03-17 00:39:23 +0100

    edit validation exception

--- src/main/java/com/persoff68/fatodo/web/rest/exception/handler/RestExceptionHandling.java
+++ src/main/java/com/persoff68/fatodo/web/rest/exception/handler/RestExceptionHandling.java
@@ -1,11 +1,30 @@
 package com.persoff68.fatodo.web.rest.exception.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.persoff68.fatodo.exception.attribute.AttributeHandler;
+import com.persoff68.fatodo.web.rest.exception.ValidationException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE + 2)
+@RequiredArgsConstructor
 public class RestExceptionHandling {
+
+    private final ObjectMapper objectMapper;
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleAbstractException(HttpServletRequest request) throws IOException {
+        Exception e = new ValidationException();
+        return AttributeHandler.from(request, e).getResponseEntity(objectMapper);
+    }
 
 }
