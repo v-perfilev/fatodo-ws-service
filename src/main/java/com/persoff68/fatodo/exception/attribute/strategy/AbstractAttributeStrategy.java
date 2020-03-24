e7e7b5fdd81b224b8ed4b0bd75079af5609835b9
Vladimir Perfilev <persoff68@gmail.com>
2020-03-24 19:29:57 +0100

    edit attribute strategies

---
+++ src/main/java/com/persoff68/fatodo/exception/attribute/strategy/AbstractAttributeStrategy.java
@@ -0,0 +1,44 @@
+package com.persoff68.fatodo.exception.attribute.strategy;
+
+import com.persoff68.fatodo.exception.AbstractException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public abstract class AbstractAttributeStrategy implements AttributeStrategy {
+
+    protected final Exception exception;
+
+    @Getter
+    protected final Map<String, Object> errorAttributes = new LinkedHashMap<>();
+
+    @Override
+    public HttpStatus getStatus() {
+        return exception instanceof AbstractException && ((AbstractException) exception).getStatus() != null
+                ? ((AbstractException) exception).getStatus()
+                : HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    @Override
+    public void addTimestamp() {
+        errorAttributes.put("timestamp", new Date());
+    }
+
+    @Override
+    public void addStatus() {
+        HttpStatus status = getStatus();
+        errorAttributes.put("status", status.value());
+        errorAttributes.put("error", status.getReasonPhrase());
+    }
+
+    @Override
+    public void addErrorDetails() {
+        String message = exception.getMessage();
+        errorAttributes.put("message", message);
+    }
+}
--- src/main/java/com/persoff68/fatodo/exception/attribute/strategy/ExceptionAttributeStrategy.java
+++ src/main/java/com/persoff68/fatodo/exception/attribute/strategy/ExceptionAttributeStrategy.java
@@ -1,51 +1,19 @@
 package com.persoff68.fatodo.exception.attribute.strategy;
 
-import com.persoff68.fatodo.exception.AbstractException;
 import com.persoff68.fatodo.exception.attribute.util.AttributeUtils;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-@RequiredArgsConstructor
-public final class ExceptionAttributeStrategy implements AttributeStrategy {
+public final class ExceptionAttributeStrategy extends AbstractAttributeStrategy {
     private static final String REQUEST_URI_PATH = "javax.servlet.error.request_uri";
 
-    @Getter
-    protected final Map<String, Object> errorAttributes = new LinkedHashMap<>();
-
     private final HttpServletRequest request;
-    private final Exception exception;
 
-    @Override
-    public void addTimestamp() {
-        errorAttributes.put("timestamp", new Date());
-    }
-
-    @Override
-    public HttpStatus getStatus() {
-        return exception instanceof AbstractException && ((AbstractException) exception).getStatus() != null
-                ? ((AbstractException) exception).getStatus()
-                : HttpStatus.INTERNAL_SERVER_ERROR;
-    }
-
-    @Override
-    public void addStatus() {
-        HttpStatus status = getStatus();
-        errorAttributes.put("status", status.value());
-        errorAttributes.put("error", status.getReasonPhrase());
-    }
-
-    @Override
-    public void addErrorDetails() {
-        String message = exception.getMessage();
-        errorAttributes.put("message", message);
+    public ExceptionAttributeStrategy(HttpServletRequest request, Exception exception) {
+        super(exception);
+        this.request = request;
     }
 
     @Override
