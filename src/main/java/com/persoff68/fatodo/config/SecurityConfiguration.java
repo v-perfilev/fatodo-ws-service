7bb0bb49a81504f0d6f3a7f47b603bb49cc3fd39
Vladimir Perfilev <persoff68@gmail.com>
2020-03-24 23:43:09 +0100

    disable csrf

--- src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
@@ -16,6 +16,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and().httpBasic();
