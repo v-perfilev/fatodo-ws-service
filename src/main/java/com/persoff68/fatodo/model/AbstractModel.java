3f41ee7fafec1482d0dc5b21858715787b6b4abf
Vladimir Perfilev <persoff68@gmail.com>
2020-04-08 21:37:36 +0200

    TDM-35 add serializable

---
+++ src/main/java/com/persoff68/fatodo/model/AbstractModel.java
@@ -0,0 +1,7 @@
+package com.persoff68.fatodo.model;
+
+import java.io.Serializable;
+
+public abstract class AbstractModel implements Serializable {
+}
+
---
+++ src/main/java/com/persoff68/fatodo/model/dto/AbstractDTO.java
@@ -0,0 +1,6 @@
+package com.persoff68.fatodo.model.dto;
+
+import java.io.Serializable;
+
+public abstract class AbstractDTO implements Serializable {
+}
