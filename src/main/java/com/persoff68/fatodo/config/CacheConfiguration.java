50c6badb46eaba0566cfa88902105298858c8a74
Vladimir Perfilev <persoff68@gmail.com>
2020-04-08 20:00:25 +0200

    TDM-35 add redis cache

---
+++ src/main/java/com/persoff68/fatodo/config/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package com.persoff68.fatodo.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+}
