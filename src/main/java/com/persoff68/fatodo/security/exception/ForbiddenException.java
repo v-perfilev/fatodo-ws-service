4a6a25ca0969743b6dbc1c79ee32069b87d33e45
Vladimir Perfilev <persoff68@gmail.com>
2020-04-26 23:01:57 +0200

    TDM-44 add feedback to security exceptions

--- src/main/java/com/persoff68/fatodo/security/exception/ForbiddenException.java
+++ src/main/java/com/persoff68/fatodo/security/exception/ForbiddenException.java
@@ -4,8 +4,10 @@ import com.persoff68.fatodo.exception.AbstractException;
 import org.springframework.http.HttpStatus;
 
 public class ForbiddenException extends AbstractException {
+    private static final String MESSAGE = "Access forbidden";
+    private static final String FEEDBACK_CODE = "security.forbidden";
 
     public ForbiddenException() {
-        super(HttpStatus.FORBIDDEN, "Access is denied");
+        super(HttpStatus.FORBIDDEN, MESSAGE, FEEDBACK_CODE);
     }
 }
--- src/main/java/com/persoff68/fatodo/security/exception/UnauthorizedException.java
+++ src/main/java/com/persoff68/fatodo/security/exception/UnauthorizedException.java
@@ -4,8 +4,10 @@ import com.persoff68.fatodo.exception.AbstractException;
 import org.springframework.http.HttpStatus;
 
 public class UnauthorizedException extends AbstractException {
+    private static final String MESSAGE = "Authentication required";
+    private static final String FEEDBACK_CODE = "security.unauthorized";
 
     public UnauthorizedException() {
-        super(HttpStatus.UNAUTHORIZED, "Full authentication required");
+        super(HttpStatus.UNAUTHORIZED, MESSAGE, FEEDBACK_CODE);
     }
 }
