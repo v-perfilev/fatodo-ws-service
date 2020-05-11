deb0f14c50f2e17a2b08c6b278d3209be8740936
Vladimir Perfilev <persoff68@gmail.com>
2020-05-11 21:29:23 +0200

    TDM-46 add swagger-ui

--- src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/SecurityConfiguration.java
@@ -54,6 +54,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterAfter(securityLocaleFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
                 .anyRequest().authenticated();
     }
 
