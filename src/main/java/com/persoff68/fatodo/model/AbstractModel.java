7ebfa735373d53ad09cdc156099d4571eeaa84ac
Vladimir Perfilev <persoff68@gmail.com>
2020-09-28 20:26:27 +0200

    edit abstract models

--- src/main/java/com/persoff68/fatodo/model/AbstractModel.java
+++ src/main/java/com/persoff68/fatodo/model/AbstractModel.java
@@ -1,7 +1,15 @@
 package com.persoff68.fatodo.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
 import java.io.Serializable;
 
+@Data
 public abstract class AbstractModel implements Serializable {
+
+    @Id
+    protected String id;
+
 }
 
--- src/main/java/com/persoff68/fatodo/model/dto/AbstractDTO.java
+++ src/main/java/com/persoff68/fatodo/model/dto/AbstractDTO.java
@@ -1,6 +1,14 @@
 package com.persoff68.fatodo.model.dto;
 
+import com.persoff68.fatodo.config.constant.AppConstants;
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public abstract class AbstractDTO implements Serializable {
+    protected static final long serialVersionUID = AppConstants.SERIAL_VERSION_UID;
+
+    protected String id;
+
 }
