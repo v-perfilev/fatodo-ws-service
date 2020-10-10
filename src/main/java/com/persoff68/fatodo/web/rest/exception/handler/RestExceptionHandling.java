fecc992ac86c33e6bf6d16bd4cede4c979928f22
Vladimir Perfilev <persoff68@gmail.com>
2020-10-10 17:44:53 +0200

    TDM-68 add BindException handler

--- src/main/java/com/persoff68/fatodo/web/rest/exception/handler/RestExceptionHandling.java
+++ src/main/java/com/persoff68/fatodo/web/rest/exception/handler/RestExceptionHandling.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,7 +23,13 @@ public class RestExceptionHandling {
     private final ObjectMapper objectMapper;
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleAbstractException(HttpServletRequest request) throws IOException {
+    public ResponseEntity<String> handleMethodArgumentNotValidException(HttpServletRequest request) throws IOException {
+        Exception e = new InvalidFormException();
+        return AttributeHandler.from(request, e).getResponseEntity(objectMapper);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<String> handleBindException(HttpServletRequest request) throws IOException {
         Exception e = new InvalidFormException();
         return AttributeHandler.from(request, e).getResponseEntity(objectMapper);
     }
