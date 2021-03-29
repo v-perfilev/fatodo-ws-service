97a781bf12a1b84b4cb42acc2ad72399ee276ec7
Vladimir Perfilev <persoff68@gmail.com>
2021-03-29 23:56:47 +0200

    TDM-83 add jackson guava dependency and set jackson config

--- src/main/java/com/persoff68/fatodo/config/JacksonConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/JacksonConfiguration.java
@@ -1,8 +1,10 @@
 package com.persoff68.fatodo.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,14 +15,20 @@ import org.springframework.context.annotation.Configuration;
 public class JacksonConfiguration {
 
     @Bean
-    JavaTimeModule javaTimeModule() {
+    Module javaTimeModule() {
         return new JavaTimeModule();
+    }
+
+    @Bean
+    Module guavaModule() {
+        return new GuavaModule();
     }
 
     @Bean
     ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(javaTimeModule());
+        objectMapper.registerModule(guavaModule());
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         return objectMapper;
