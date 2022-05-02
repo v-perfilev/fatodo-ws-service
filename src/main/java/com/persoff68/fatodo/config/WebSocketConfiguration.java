0a195c145614045e7027e686fe45034e6445047b
Vladimir Perfilev <persoff68@gmail.com>
2022-05-03 00:01:41 +0200

    TDM-132 fix web socket security bug

--- src/main/java/com/persoff68/fatodo/config/WebSocketConfiguration.java
+++ src/main/java/com/persoff68/fatodo/config/WebSocketConfiguration.java
@@ -23,7 +23,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
