eb7472cd28f6246b5681b18143d175470ad6974b
Vladimir Perfilev <persoff68@gmail.com>
2022-07-17 17:15:22 +0200

    TDM-149 edit feign configurations

--- src/main/java/com/persoff68/fatodo/client/UserServiceClient.java
+++ src/main/java/com/persoff68/fatodo/client/UserServiceClient.java
@@ -1,5 +1,6 @@
 package com.persoff68.fatodo.client;
 
+import com.persoff68.fatodo.client.configuration.FeignSystemConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "user-service", primary = false, qualifiers = {"feignUserServiceClient"})
+@FeignClient(name = "user-service", primary = false,
+        configuration = {FeignSystemConfiguration.class},
+        qualifiers = {"feignUserServiceClient"})
 public interface UserServiceClient {
 
     @PostMapping(value = "/api/user-data/usernames/ids")
