12759d51a422bc905243303e299e8cb8c5d890ec
Vladimir Perfilev <persoff68@gmail.com>
2022-07-20 19:52:29 +0200

    TDM-149 edit logging aspects

--- src/main/java/com/persoff68/fatodo/config/aop/logging/LoggingAspect.java
+++ src/main/java/com/persoff68/fatodo/config/aop/logging/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.persoff68.fatodo.config.aop.logging;
 
 import com.persoff68.fatodo.config.constant.AppConstants;
+import com.persoff68.fatodo.exception.AbstractException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -17,32 +18,39 @@ import java.util.Arrays;
 @Slf4j
 public class LoggingAspect {
     private static final String CONTROLLER_POINTCUT = "within(" + AppConstants.CONTROLLER_PATH + "..*)";
-    private static final String SERVICE_POINTCUT = "within(" + AppConstants.SERVICE_PATH + "..*)";
+    private static final String TASK_POINTCUT = "within(" + AppConstants.TASK_PATH + "..*)";
 
-    @Pointcut(CONTROLLER_POINTCUT + " || "
-            + SERVICE_POINTCUT)
+    @Pointcut(CONTROLLER_POINTCUT + " || " + TASK_POINTCUT)
     public void applicationPackagePointcut() {
         // pointcut for controllers and services
     }
 
     @AfterThrowing(pointcut = "applicationPackagePointcut()", throwing = "e")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName(), e.getMessage() != null ? e.getMessage() : "NULL");
+        String declaringTypeName = joinPoint.getSignature().getDeclaringTypeName();
+        String name = joinPoint.getSignature().getName();
+        String message = e.getMessage() != null ? e.getMessage() : "NULL";
+
+        if (e instanceof AbstractException) {
+            log.warn("Exception in {}.{}() with cause = {}", declaringTypeName, name, message);
+        } else {
+            log.error("Exception in {}.{}() with cause = {}", declaringTypeName, name, message);
+        }
     }
 
     @Around("applicationPackagePointcut()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.debug("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        String declaringTypeName = joinPoint.getSignature().getDeclaringTypeName();
+        String name = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        log.debug("Enter: {}.{}() with argument[s] = {}", declaringTypeName, name, args);
         try {
             Object result = joinPoint.proceed();
-            log.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(), result);
+            log.debug("Exit: {}.{}() with result = {}", declaringTypeName, name, result);
             return result;
         } catch (IllegalArgumentException e) {
-            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
-                    joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+            log.error("Illegal argument: {} in {}.{}()", args, declaringTypeName, name);
             throw e;
         }
     }
