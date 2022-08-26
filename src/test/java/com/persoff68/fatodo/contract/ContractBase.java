f39ec93caa4fda270f59a8b3ccfb04b4ee58a41b
Vladimir Perfilev <persoff68@gmail.com>
2022-08-26 22:08:45 +0200

    TDM-153 fix tests

--- src/test/java/com/persoff68/fatodo/contract/ContractBase.java
+++ src/test/java/com/persoff68/fatodo/contract/ContractBase.java
@@ -1,38 +1,83 @@
 package com.persoff68.fatodo.contract;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.persoff68.fatodo.builder.TestItemInfo;
 import com.persoff68.fatodo.builder.TestUserInfo;
+import com.persoff68.fatodo.client.ItemServiceClient;
 import com.persoff68.fatodo.client.UserServiceClient;
+import com.persoff68.fatodo.model.ItemInfo;
 import com.persoff68.fatodo.model.UserInfo;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpSession;
+import org.springframework.messaging.simp.user.SimpSubscription;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMessageVerifier
 class ContractBase {
 
+    private static final UUID ACTIVE_USER_ID = UUID.randomUUID();
+    private static final String ACTIVE_USER_NAME = "active";
+    private static final UUID INACTIVE_USER_ID = UUID.randomUUID();
+    private static final String INACTIVE_USER_NAME = "inactive";
+
     @Autowired
     WebApplicationContext context;
 
     @MockBean
     UserServiceClient userServiceClient;
+    @MockBean
+    ItemServiceClient itemServiceClient;
+    @MockBean
+    SimpMessagingTemplate messagingTemplate;
+    @MockBean
+    SimpUserRegistry userRegistry;
+    @MockBean
+    FirebaseMessaging firebaseMessaging;
+
 
     @BeforeEach
     void setup() {
         RestAssuredMockMvc.webAppContextSetup(context);
 
-        UserInfo userInfo = TestUserInfo.defaultBuilder().build().toParent();
-        List<UserInfo> userInfoList = List.of(userInfo);
-        when(userServiceClient.getAllUserInfoByIds(any())).thenReturn(userInfoList);
+        UserInfo activeUserInfo = TestUserInfo.defaultBuilder()
+                .id(ACTIVE_USER_ID).username(ACTIVE_USER_NAME).build().toParent();
+        UserInfo inactiveUserInfo = TestUserInfo.defaultBuilder()
+                .id(INACTIVE_USER_ID).username(INACTIVE_USER_NAME).language("ru").build().toParent();
+        when(userServiceClient.getAllUserInfoByIds(eq(List.of(ACTIVE_USER_ID))))
+                .thenReturn(List.of(activeUserInfo));
+        when(userServiceClient.getAllUserInfoByIds(eq(List.of(INACTIVE_USER_ID))))
+                .thenReturn(List.of(inactiveUserInfo));
+        when(userServiceClient.getAllUserInfoByIds(any()))
+                .thenReturn(List.of(activeUserInfo, inactiveUserInfo));
+
+        ItemInfo itemInfo = TestItemInfo.defaultBuilder().build().toParent();
+        when(itemServiceClient.getAllItemInfoByIds(any())).thenReturn(List.of(itemInfo));
+
+        SimpSubscription simpSubscription = Mockito.mock(SimpSubscription.class);
+        SimpSession simpSession = Mockito.mock(SimpSession.class);
+        SimpUser simpUser = Mockito.mock(SimpUser.class);
+        when(simpUser.getName()).thenReturn(ACTIVE_USER_NAME);
+        when(simpSession.getUser()).thenReturn(simpUser);
+        when(simpSubscription.getSession()).thenReturn(simpSession);
+        when(userRegistry.findSubscriptions(any())).thenReturn(Set.of(simpSubscription));
     }
 
 }
--- src/test/java/com/persoff68/fatodo/web/kafka/EventConsumerIT.java
+++ src/test/java/com/persoff68/fatodo/web/kafka/EventConsumerIT.java
@@ -1,11 +1,15 @@
 package com.persoff68.fatodo.web.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.persoff68.fatodo.builder.TestContactRequest;
+import com.persoff68.fatodo.builder.TestItemInfo;
 import com.persoff68.fatodo.builder.TestUserInfo;
 import com.persoff68.fatodo.builder.TestWsEvent;
+import com.persoff68.fatodo.client.ItemServiceClient;
 import com.persoff68.fatodo.client.UserServiceClient;
 import com.persoff68.fatodo.config.util.KafkaUtils;
+import com.persoff68.fatodo.model.ItemInfo;
 import com.persoff68.fatodo.model.UserInfo;
 import com.persoff68.fatodo.model.WsEvent;
 import com.persoff68.fatodo.model.constant.WsDestination;
@@ -14,6 +18,7 @@ import com.persoff68.fatodo.model.event.ContactRequest;
 import com.persoff68.fatodo.service.WsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -21,13 +26,21 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.user.SimpSession;
+import org.springframework.messaging.simp.user.SimpSubscription;
+import org.springframework.messaging.simp.user.SimpUser;
+import org.springframework.messaging.simp.user.SimpUserRegistry;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.startsWith;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -42,6 +55,11 @@ import static org.mockito.Mockito.when;
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 class EventConsumerIT {
 
+    private static final UUID ACTIVE_USER_ID = UUID.randomUUID();
+    private static final String ACTIVE_USER_NAME = "active";
+    private static final UUID INACTIVE_USER_ID = UUID.randomUUID();
+    private static final String INACTIVE_USER_NAME = "inactive";
+
     @Autowired
     private EmbeddedKafkaBroker embeddedKafkaBroker;
 
@@ -54,6 +72,14 @@ class EventConsumerIT {
 
     @MockBean
     UserServiceClient userServiceClient;
+    @MockBean
+    ItemServiceClient itemServiceClient;
+    @MockBean
+    SimpMessagingTemplate messagingTemplate;
+    @MockBean
+    SimpUserRegistry userRegistry;
+    @MockBean
+    FirebaseMessaging firebaseMessaging;
 
     private KafkaTemplate<String, WsEvent> wsKafkaTemplate;
 
@@ -61,9 +87,27 @@ class EventConsumerIT {
     void setup() {
         wsKafkaTemplate = buildKafkaTemplate();
 
-        UserInfo userInfo = TestUserInfo.defaultBuilder().build().toParent();
-        List<UserInfo> userInfoList = List.of(userInfo);
-        when(userServiceClient.getAllUserInfoByIds(any())).thenReturn(userInfoList);
+        UserInfo activeUserInfo = TestUserInfo.defaultBuilder()
+                .id(ACTIVE_USER_ID).username(ACTIVE_USER_NAME).build().toParent();
+        UserInfo inactiveUserInfo = TestUserInfo.defaultBuilder()
+                .id(INACTIVE_USER_ID).username(INACTIVE_USER_NAME).language("ru").build().toParent();
+        when(userServiceClient.getAllUserInfoByIds(eq(List.of(ACTIVE_USER_ID))))
+                .thenReturn(List.of(activeUserInfo));
+        when(userServiceClient.getAllUserInfoByIds(eq(List.of(INACTIVE_USER_ID))))
+                .thenReturn(List.of(inactiveUserInfo));
+        when(userServiceClient.getAllUserInfoByIds(any()))
+                .thenReturn(List.of(activeUserInfo, inactiveUserInfo));
+
+        ItemInfo itemInfo = TestItemInfo.defaultBuilder().build().toParent();
+        when(itemServiceClient.getAllItemInfoByIds(any())).thenReturn(List.of(itemInfo));
+
+        SimpSubscription simpSubscription = Mockito.mock(SimpSubscription.class);
+        SimpSession simpSession = Mockito.mock(SimpSession.class);
+        SimpUser simpUser = Mockito.mock(SimpUser.class);
+        when(simpUser.getName()).thenReturn(ACTIVE_USER_NAME);
+        when(simpSession.getUser()).thenReturn(simpUser);
+        when(simpSubscription.getSession()).thenReturn(simpSession);
+        when(userRegistry.findSubscriptions(any())).thenReturn(Set.of(simpSubscription));
     }
 
     @Test
